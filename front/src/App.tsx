94b33fcfd3ed9993eb4fe57986ab631be1ee1693
James Mitofsky <JamesMitofsky@gmail.com>
2024-02-14 11:48:39 +0100

    Initialize nav bar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,21 +1,27 @@
 import CountBtn from "@/components/CountBtn";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "./components/ui/button";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center gap-y-4">
-        <div className="inline-flex items-center gap-x-4"></div>
-        <a
-          href="https://ui.shadcn.com"
-          rel="noopener noreferrer nofollow"
-          target="_blank"
-        >
-          <Badge variant="outline">shadcn/ui</Badge>
-        </a>
-        <CountBtn />
-      </div>
-    </main>
+    <>
+      <NavBar />
+      <main className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center gap-y-4">
+          <div className="inline-flex items-center gap-x-4"></div>
+          <a
+            href="https://ui.shadcn.com"
+            rel="noopener noreferrer nofollow"
+            target="_blank"
+          >
+            <Button variant="outline">shadcn/ui</Button>
+            <Badge variant="outline">shadcn/ui</Badge>
+          </a>
+          <CountBtn />
+        </div>
+      </main>
+    </>
   );
 }
 
