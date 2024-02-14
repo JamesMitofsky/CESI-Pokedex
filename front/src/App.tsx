5694aec706e58e776217b64167c8fdc92baee079
James Mitofsky <JamesMitofsky@gmail.com>
2024-02-14 15:59:16 +0100

    Remove default components & tailwind

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,3 @@
-import CountBtn from "@/components/CountBtn";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "./components/ui/button";
 import NavBar from "./components/NavBar";
 import PokedexCard from "./components/PokedexCard";
 import { Creature } from "../types/creature";
@@ -43,20 +40,9 @@ function App() {
     <>
       <NavBar />
       <main className="flex flex-col items-center justify-center h-screen">
-        <div className="flex flex-col items-center gap-y-4">
-          <div className="inline-flex items-center gap-x-4"></div>
-          <a
-            href="https://ui.shadcn.com"
-            rel="noopener noreferrer nofollow"
-            target="_blank"
-          >
-            <Button variant="outline">shadcn/ui</Button>
-            <Badge variant="outline">shadcn/ui</Badge>
-          </a>
-          <CountBtn />
+
         <PokedexCard creature={creatures[0]} />
- 
-        </div>
+
       </main>
     </>
   );
