1f8ab489ef3490d12d2fe4900603af85041836a5
KevinMalle <80816585+KevinMalle@users.noreply.github.com>
2024-02-14 12:06:43 +0100

    ajout de boutons et import d'avatar

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function NavBar() {
-  return <header>this will be a nav bar</header>;
+  return <header>
+    this will be a nav bar
+    <Button variant="ghost">Accueil</Button>
+    <Button variant="ghost">Accueil</Button>
+    <Button variant="ghost">Mon Pokédex</Button>
+    </header>
+  ;
 }
