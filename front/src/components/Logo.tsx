dbd2398f0266ab248f52b626624fa2a047ceba0e
KevinMalle <80816585+KevinMalle@users.noreply.github.com>
2024-02-14 14:42:08 +0100

    stylisation de la navbar

--- front/src/components/Logo.tsx
+++ front/src/components/Logo.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
 
 export default function Logo() {
 return (
-<Avatar>
+<Avatar className="h-20 w-22">
   <AvatarImage src="/images/palworld-logo.png" />
   <AvatarFallback>CN</AvatarFallback>
 </Avatar>
--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -3,20 +3,20 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Logo from "@/components/Logo";
 
 export default function NavBar() {
-  return (<header className="navbar">
-    
-    <Logo/>
-
-    <p>this will be a great nav bar, you just haven't seen it yet</p>
-
-    <Button variant="ghost">Accueil</Button>
-
-    <Button variant="ghost">Inscription</Button>
-
-    <Avatar>
-      <AvatarImage src="https://github.com/shadcn.png" />
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
+  return (
+    <header className="flex items-center justify-between p-4">
+      <div className="flex items-center space-x-4">
+        <Logo/>
+      </div>
+      <p className="text-gray-600">This will be a great nav bar, you just haven't seen it yet</p>
+      <div className="flex items-center space-x-4">
+        <Button variant="ghost">Accueil</Button>
+        <Button variant="ghost">Inscription</Button>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
     </header>
   );
 }
