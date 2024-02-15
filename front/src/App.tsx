d5ec47881a22e9629b19f1ffd0d6d604c0235c84
James Mitofsky <JamesMitofsky@gmail.com>
2024-02-15 09:08:33 +0100

    Add MVP of dialog for each pokedex card.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,8 @@
 import NavBar from "./components/NavBar";
 import PokedexCard from "./components/PokedexCard";
 import { Creature } from "../types/creature";
+import { useState, useEffect } from 'react';
+import CreatureDialog from "./components/CreatureDialog";
 
 const creatures: Creature[] = [
   {
@@ -36,21 +38,38 @@ const creatures: Creature[] = [
 
 
 function App() {
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [selectedCreature, setSelectedCreature] = useState<Creature | null>(null);
+
+  useEffect(() => {
+    if (selectedCreature) {
+      setIsDialogOpen(true);
+    }
+  }, [selectedCreature]);
+  useEffect(() => {
+    if (!isDialogOpen) {
+      setSelectedCreature(null);
+    }
+  }, [isDialogOpen]);
+
   return (
     <>
       <NavBar />
       <main className="flex flex-col items-center justify-center h-screen">
         <div className="flex flex-wrap gap-4">
       {creatures.map((creature) => (
-        <div>
-          <PokedexCard creature={creature} key={creature.id} />
-        </div>
+
+          <PokedexCard creature={creature} key={creature.id} setSelectedCreature={setSelectedCreature} />
+
       ))}
       </div>
 
       </main>
+      <CreatureDialog open={isDialogOpen} setOpen={setIsDialogOpen} creature={selectedCreature} />
     </>
   );
 }
 
 export default App;
+
