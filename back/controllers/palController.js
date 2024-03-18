54e430e4aeff47711010cc66cc0f107389b221f8
Finlais <arnaud.le-cozannet@orange.fr>
2024-03-18 09:31:51 +0100

    feat: Init back + palController

--- back/controllers/palController.js
+++ back/controllers/palController.js
@@ -2,6 +2,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 exports.getPals = async (req, res) => {
-  const pals = await prisma.pal.findMany();
+  const pals = await prisma.pals.findMany();
   res.json(pals);
 };
