13c8b51bc034f0c00132a8d3dcf293812ed2f103
James Mitofsky <JamesMitofsky@gmail.com>
2024-03-19 15:50:48 +0100

    Fix: correct the shape of data in the dialog.

--- front/src/components/CreatureDialog.tsx
+++ front/src/components/CreatureDialog.tsx
@@ -1,11 +1,20 @@
 import { Creature } from "types/creature";
 import {
-    Dialog, DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogDescription
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
 } from "./ui/dialog";
-export default function CreatureDialog({open, setOpen, creature}: {open: boolean, setOpen: (open: boolean) => void, creature: Creature | null}) {
+export default function CreatureDialog({
+  open,
+  setOpen,
+  creature,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  creature: Creature | null;
+}) {
   return (
     <>
       {creature && (
@@ -14,13 +23,43 @@ export default function CreatureDialog({open, setOpen, creature}: {open: boolean
             <DialogHeader>
               <DialogTitle>{creature.name}</DialogTitle>
               <DialogDescription>
-                <div>Element: {creature.element}</div>
-                <div>Creature number: {creature.number}</div>
-                <div>Skills: {creature.partnerSkill}</div>
                 <div>
-                  Drops when defeated:
-                  {creature.drop.map((drop) => (
-                    <div key={creature.id}>{drop}</div>
+                  <span className="font-bold text-lg	text-gray-100	">
+                    Element :
+                  </span>
+                  <span className="italic text-lg">
+                    {creature.elements.map((element) => (
+                      <span>{element.name}</span>
+                    ))}
+                  </span>
+                </div>
+                <div>
+                  <span className="font-bold text-lg	text-gray-100	">
+                    Creature number :
+                  </span>
+                  <span className="italic text-lg">{creature.number}</span>
+                </div>
+                <div>
+                  <span className="font-bold text-lg	text-gray-100	">
+                    Skills :
+                  </span>
+                  <span className="italic text-lg">
+                    {creature.partnerSkill.libelle}
+                  </span>
+                </div>
+                {/* <img
+                  className="absolute top-0 right-0 m-5 w-20 rounded-lg"
+                  src={`../../src/assets/${creature.name}.png`}
+                  alt={creature.name}
+                /> */}
+                <div>
+                  <span className="font-bold text-lg	text-gray-100	">
+                    Drops when defeated :
+                  </span>
+                  {creature.drops.map((drop) => (
+                    <li className="italic text-lg" key={drop.idDrop}>
+                      {drop.name}
+                    </li>
                   ))}
                 </div>
               </DialogDescription>
--- front/src/components/PokedexCard.tsx
+++ front/src/components/PokedexCard.tsx
@@ -15,15 +15,19 @@ export default function PokedexCard({
   creature: Creature;
   setSelectedCreature: (creature: Creature) => void;
 }) {
+
+  function handleClick() {
+    setSelectedCreature(creature);
+  }
   return (
-    <Card className="flex flex-col w-96 m-2 relative" onClick={() => setSelectedCreature(creature)}>
+    <Card className="flex flex-col w-96 m-2 relative" onClick={handleClick}>
       <CardHeader>
         <CardTitle className="mb-5">{creature.name}</CardTitle>
         <CardDescription>
           <div><span className="font-bold text-lg	text-gray-100	"> Element : </span> <span className="italic text-lg">{creature.elements.map(element => <span>{element.name}</span>)}</span> </div>
           <div><span className="font-bold text-lg	text-gray-100	"> Creature number : </span ><span className="italic text-lg">{creature.number}</span></div>
           <div><span className="font-bold text-lg	text-gray-100	"> Skills : </span><span  className="italic text-lg">{creature.partnerSkill.libelle}</span></div>
-          <img className="absolute top-0 right-0 m-5 w-20 rounded-lg" src={`../../src/assets/${creature.name}.png`} alt={creature.name} />
+          {/* <img className="absolute top-0 right-0 m-5 w-20 rounded-lg" src={`../../src/assets/${creature.name}.png`} alt={creature.name} /> */}
           <div>
           <span className="font-bold text-lg	text-gray-100	">Drops when defeated :</span >
             {creature.drops.map((drop) => (
