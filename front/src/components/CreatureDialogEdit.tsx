d925a5b0c60b4b7f436f204eb832aec1f493f5e3
KevinMalle <80816585+KevinMalle@users.noreply.github.com>
2024-03-19 17:03:23 +0100

    testModifButton

--- front/src/components/CreatureDialogEdit.tsx
+++ front/src/components/CreatureDialogEdit.tsx
@@ -2,14 +2,6 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
-import { Creature } from "types/creature";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-} from "./ui/dialog";
 
 import { Button } from "@/components/ui/button"
 import {
@@ -22,37 +14,29 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 
-export default function CreatureDialog({
-  open,
-  setOpen,
-  creature,
-}: {
-  open: boolean;
-  setOpen: (open: boolean) => void;
-  creature: Creature | null;
-}) {
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+   
+  }),
+  element: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+   
+  }),
+  creatureNumber: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+   
+  }),
 
-    const formSchema = z.object({
-        name: z.string().min(2, {
-          message: "Username must be at least 2 characters.",
-         
-        }),
-        element: z.string().min(2, {
-          message: "Username must be at least 2 characters.",
-         
-        }),
-        creatureNumber: z.string().min(2, {
-          message: "Username must be at least 2 characters.",
-         
-        }),
-      
-          skills: z.string().min(2, {  message: "Username must be at least 2 characters.",}),
-          dropsWhenDefeated: z.string().min(2, {  message: "Username must be at least 2 characters.",}),
-          worksForName: z.string().min(2, {  message: "Username must be at least 2 characters.",}),
-          worksForLevel: z.string().min(2, {  message: "Username must be at least 2 characters.",}),
-      })
+    skills: z.string().min(2, {  message: "Username must be at least 2 characters.",}),
+    dropsWhenDefeated: z.string().min(2, {  message: "Username must be at least 2 characters.",}),
+    worksForName: z.string().min(2, {  message: "Username must be at least 2 characters.",}),
+    worksForLevel: z.string().min(2, {  message: "Username must be at least 2 characters.",}),
+})
 
-
+export default function CreatureDialogEdit( props ) {
+  const pal = props.creature
+  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -64,146 +48,118 @@ export default function CreatureDialog({
       worksForName: "",
       worksForLevel: "",
     },
-  });
+  })
 
+  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values);
+    console.log(values)
+    props.setIsModifie(false)
   }
+
   return (
-    <>
-      {creature && (
-        <Dialog open={open} onOpenChange={setOpen}>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>{creature.name}</DialogTitle>
-              <DialogDescription>
-                <div>Element: {creature.element}</div>
-                <div>Creature number: {creature.number}</div>
-                <div>Skills: {creature.partnerSkill}</div>
-                <div>
-                  Drops when defeated:
-                  {creature.drop.map((drop) => (
-                    <div key={creature.id}>{drop}</div>
-                  ))}
-                </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>{pal.name}</FormLabel>
+              <FormControl>
+                <Input placeholder={pal.name} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-                <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-8"
-                  >
-                    <FormField
-                      control={form.control}
-                      name="name"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>name</FormLabel>
-                          <FormControl>
-                            <Input placeholder={creature.name} {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
+        <FormField
+          control={form.control}
+          name="element"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Element</FormLabel>
+              <FormControl>
+                <Input placeholder="Element" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="creatureNumber"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Creature Number</FormLabel>
+              <FormControl>
+                <Input placeholder="Numero de Pal?" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-                    <FormField
-                      control={form.control}
-                      name="element"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Element</FormLabel>
-                          <FormControl>
-                            <Input placeholder={creature.element} {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="creatureNumber"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Creature Number</FormLabel>
-                          <FormControl>
-                            <Input placeholder={creature.number} {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
+        <FormField
+          control={form.control}
+          name="skills"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Skills</FormLabel>
+              <FormControl>
+                <Input placeholder="Skills du Pal?" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-                    <FormField
-                      control={form.control}
-                      name="skills"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Skills</FormLabel>
-                          <FormControl>
-                            <Input placeholder={creature.partnerSkill} {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
+        <FormField
+          control={form.control}
+          name="dropsWhenDefeated"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Drops when Defeated</FormLabel>
+              <FormControl>
+                <Input placeholder="Quels drops?" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-                    <FormField
-                      control={form.control}
-                      name="dropsWhenDefeated"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Drops when Defeated</FormLabel>
-                          <FormControl>
-                            <Input placeholder={creature.drop} {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
+        <FormField
+          control={form.control}
+          name="worksForName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Works For Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Pour qui il travaille ce Pal?" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="worksForLevel"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Level of the Kingdom</FormLabel>
+              <FormControl>
+                <Input placeholder="Quelle niveau du Royaume?" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-                    <FormField
-                      control={form.control}
-                      name="worksForName"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Works For Name</FormLabel>
-                          <FormControl>
-                            <Input
-                              placeholder="Pour qui il travaille ce Pal?"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="worksForLevel"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Level of the Kingdom</FormLabel>
-                          <FormControl>
-                            <Input
-                              placeholder="Quelle niveau du Royaume?"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
 
-                    <Button type="submit">Submit</Button>
-                  </form>
-                </Form>
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-      )}
-    </>
-  );
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
 }
--- front/src/components/EditPalButton.tsx
+++ front/src/components/EditPalButton.tsx
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom";
 import EditSignSvg from "../assets/edit-button.svg";
+import { Creature } from "../../types/creature";
+import { useState } from "react";
 
-export default function EditPalButton() {
+
+export default function EditPalButton({
+    creature,
+    setSelectedCreature,
+    setIsModifie
+  }: {
+    creature: Creature;
+    setSelectedCreature: (creature: Creature) => void;
+  }) {
+
+   
   return (
 
     <div className="flex justify-center items-center bottom-0 left-0 right-0 p-4">
-      <Link
+      <button
         className="flex justify-center items-center text-3xl bg-blue-500 hover:bg-blue-700 font-bold p-3 rounded-full"
-        to="/modifier"
+        onClick={() => {setSelectedCreature(creature), setIsModifie(true)}}
+        // onClick={() => }
       >
         <img src={EditSignSvg} className="w-10" alt="Modifier"/>
-      </Link>
+      </button>
     </div>
   );
 }
